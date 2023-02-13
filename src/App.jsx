9c5a27d6ade4cdb75ab56a4ba97bdacc3a5d8d39
t-flim <fulim1988@gmail.com>
2023-02-13 15:27:00 +0800

    Import Header to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 
 // components
+import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 
 function App() {
 
   return (
     <div className="app">
+      <Header />
       <Footer />
     </div>
   )
