6cfc43a62d8d9e016d8dfb846570943354392a44
t-flim <fulim1988@gmail.com>
2023-02-13 15:29:04 +0800

    Edit Header structure code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,13 @@ import "./Header.css"
 import {hero} from "../../info/portfolio-data"
 
 function Header() {
-    const {firstName, lastName, location} = hero
+    const {firstName, lastName, title, location} = hero
     
     return(
         <header id="hero">
             <div class="hero-name">
                 <h1>{firstName} {lastName}</h1>
-                <p></p>
+                <h4>{title}</h4>
             </div>
             <p>{location}</p>
         </header>
