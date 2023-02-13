aee057d75949a062344eb286d9648aa04ecc7d27
t-flim <fulim1988@gmail.com>
2023-02-13 14:45:16 +0800

    Add portfolio data file

---
+++ src/info/portfolio-data.js
@@ -0,0 +1,29 @@
+const hero = {
+    firstName: "Fu Lim",
+    lastName: "Tey",
+    location: "Kuala Lumpur",
+}
+
+const projects = []
+
+const techStack = [
+    {id: 1, name: "html", url: "html5-plain"},
+    {id: 2, name: "css", url: "css3-plain"},
+    {id: 3, name: "bootstrap", url: "bootstrap-plain"},
+    {id: 4, name: "javascript", url: "javascript-plain"},
+    {id: 5, name: "jquery", url: "jquery-plain"},
+    {id: 6, name: "reactjs", url: "react-original"},
+    {id: 7, name: "nodejs", url: "nodejs-plain"},
+    {id: 8, name: "expressjs", url: "express-original"},
+    {id: 9, name: "mongodb", url: "mongdb-plain"},
+    {id: 10, name: "git",  url: "git-plain"},
+]
+
+const contactMethods = [
+    {id: 1, method: "email", url: "fulim1988@gmail.com"},
+    {id: 2, method: "github", url: "https://github.com/t-flim"},
+]
+
+
+
+export {hero, projects, techStack, contactMethods}
