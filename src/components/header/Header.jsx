1108c062294c8ec2b6629cd4f9659b3e18b9a503
t-flim <fulim1988@gmail.com>
2023-02-13 15:25:07 +0800

    Import hero data to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import "./Header.css"
+import {hero} from "../../info/portfolio-data"
 
 function Header() {
+    const {firstName, lastName, location} = hero
     
     return(
-        <header id="header"></header>
+        <header id="hero">
+            <div class="hero-name">
+                <h1>{firstName} {lastName}</h1>
+                <p></p>
+            </div>
+            <p>{location}</p>
+        </header>
     )
 }
 
