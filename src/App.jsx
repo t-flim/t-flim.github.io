7a8f5b7768c95bebc6654d235a368b10e1d8eac7
t-flim <fulim1988@gmail.com>
2023-02-14 10:34:32 +0800

    Import Projects to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css"
 
 // components
 import Header from "./components/header/Header"
+import Projects from "./components/projects/Projects"
 import TechStacks from "./components/tech-stacks/TechStacks"
 import Contacts from "./components/contacts/Contacts"
 import Footer from "./components/footer/Footer"
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <Projects />
       <TechStacks />
       <Contacts />
       <Footer />
