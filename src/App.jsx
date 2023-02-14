f331c99b40cc39a38666f20fc8ab7896f9231475
t-flim <fulim1988@gmail.com>
2023-02-14 10:05:31 +0800

    Import Contacts to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css"
 // components
 import Header from "./components/header/Header"
 import TechStacks from "./components/tech-stacks/TechStacks"
+import Contacts from "./components/contacts/Contacts"
 import Footer from "./components/footer/Footer"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="app">
       <Header />
       <TechStacks />
+      <Contacts />
       <Footer />
     </div>
   )
