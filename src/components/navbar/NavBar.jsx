27ee0f4dea2f5aa13624c85e7aed15cf6f942969
t-flim <fulim1988@gmail.com>
2023-02-14 11:24:37 +0800

    Add nav links

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,9 +1,25 @@
 import "./NavBar.css"
+import {projects, techStacks, contactMethods} from "../../info/portfolio-data"
 
 function NavBar() {
 
     return(
-        <nav id="navbar"></nav>
+        <nav id="nav">
+            <ul className="nav__list">
+                <li className="nav__list--item">
+                    <a href="#hero">Home</a>
+                </li>
+                {(projects.length > 0) && <li className="nav__list--item">
+                    <a href="#projects">Projects</a>
+                </li>}
+                {(techStacks.length > 0) && <li className="nav__list--item">
+                    <a href="#tech-stacks">Tech Stack</a>
+                </li>}
+                {(contactMethods.length > 0) && <li className="nav__list--item">
+                    <a href="#contacts">Contact</a>
+                </li>}
+            </ul>
+        </nav>
     )
 }
 
