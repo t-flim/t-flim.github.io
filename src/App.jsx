8592377789d5c5bdb47bf2a2c79f58720223252c
t-flim <fulim1988@gmail.com>
2023-02-24 09:40:36 +0800

    Reset to initial condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
 import "./App.css"
-import {useState} from "react"
 
 // components
-import NavBar from "./components/navbar/NavBar"
-import Header from "./components/header/Header"
-import Projects from "./components/projects/Projects"
-import Technologies from "./components/technologies/Technologies"
-import Contacts from "./components/contacts/Contacts"
-import Footer from "./components/footer/Footer"
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("home")
-
-  const trackPage = (i) => {
-    // console.log(i)
-    setCurrentPage(i)
-  }
-
-  let mainContent
-  switch(currentPage) {
-    case "projects": mainContent = <Projects />
-      break
-
-    case "technologies": mainContent = <Technologies />
-      break
-
-    case "contacts": mainContent = <Contacts />
-      break
-
-    default: mainContent = <Header />
-  }
 
   return (
     <div className="app">
-      <NavBar handleClick={trackPage} />
-        <main>
-          {mainContent}
-        </main>
-      <Footer />
     </div>
   )
 }
